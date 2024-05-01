962a3f6290192917ad8683d21bfd7fc87f44dbbf
Miguel Silva <miguel.figueiredo13@gmail.com>
2024-05-01 13:47:56 +0100

    updates

--- SPC_module/SPC.py
+++ SPC_module/SPC.py
@@ -15,14 +15,14 @@ class SPCAlgorithm:
         self.num_negative = 0
         self.num_examples = 0
         self.error_rates = [] # for plotting
-        self.init_estimator = init_estimator
-        self.reset_model()
+        self._init_estimator = init_estimator
+        self._reset_model()
         self.warn = -1
         self.warning_level = []
         self.drift_level = []
         self.states = []
 
-    def update(self, y):
+    def _update(self, y):
         # Update counts
         self.num_examples += 1
         if (y == False):
@@ -47,7 +47,7 @@ class SPCAlgorithm:
         self.error_rates.append(p)
         return status        
 
-    def model_train(self, data):
+    def _model_train(self, data):
         for i in range(data.shape[0]):
             try:
                 x, y = data.iloc[i, :-1], data.iloc[i, -1]
@@ -55,23 +55,23 @@ class SPCAlgorithm:
                 x, y = data[:-1], data[-1]
         self.model.learn_one(x, y)
     
-    def reset_model(self):
-        self.model = self.init_estimator()
+    def _reset_model(self):
+        self.model = self._init_estimator()
 
     def model_control(self, data, sample_id):
         x = data.iloc[sample_id, :-1]
         y = data.iloc[sample_id, -1]
         y_pred = self.model.predict_one(x)
 
-        status = self.update(y_pred==y)        
+        status = self._update(y_pred==y)        
         # check detector status
         if status == 'Warning Level' and self.warn == -1 and sample_id!=0:
             self.warn = sample_id
         elif status == 'Out-control':
-            self.reset_model()
-            self.model_train(data.iloc[self.warn:sample_id+1,:])
+            self._reset_model()
+            self._model_train(data.iloc[self.warn:sample_id+1,:])
         else:
-            self.model_train(data.iloc[sample_id,:])
+            self._model_train(data.iloc[sample_id,:])
         
         self.states.append(status_dict[status])
         return status, y, y_pred
